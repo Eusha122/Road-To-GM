fccb4cf495d1ca9a903bf7173a0deae6860b5ab7
Eusha Ibna Akbor <eushaibnaakbor@gmail.com>
2026-09-11 19:52:23 +0600

    cf problem solving.

---
+++ B_Unconventional_Pairs.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int tst;
+   cin>>tst;
+
+   while(tst--){
+
+      int a;
+      cin>>a;
+
+      vector<int> v(a);
+
+      for(int i=0; i<a; i++){
+         cin>>v[i];
+      }
+      sort(v.begin(), v.end());
+
+      vector<int> sum(a/2);
+
+      for(int i=0; i<a; i+=2){
+
+         pair<int,int> p;
+         
+         p.first = v[i];
+         p.second =v[i+1];
+         
+      }
+   }
+
+   return 0;
+}
