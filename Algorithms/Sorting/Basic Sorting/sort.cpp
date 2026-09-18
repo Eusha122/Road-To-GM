4a4d594048eac42e463c104c399d75eeee1e10ce
Eusha Ibna Akbor <eushaibnaakbor@gmail.com>
2026-09-18 22:41:41 +0600

    .

--- Algorithms/Sorting/Basic Sorting/sort.cpp
+++ Algorithms/Sorting/Basic Sorting/sort.cpp
@@ -6,9 +6,16 @@ int main() {
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
 
-   vector<int> v = {2,3,5,5,1,5,1,5,6,3,3,3,46,547,2352};
+   vector<pair<int,int>> vp = {
+      {2,4},
+      (2,5),
+      (10,3)
+   };
 
-   sort(v.begin(), v.end(), greater<int>());
+
+   sort(vp.begin(), vp.end(), [](auto a, auto b){
+      return a.f>b;
+   });
 
    for(auto i : v){
       cout<<i<<endl;
